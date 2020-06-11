79c72d56601e7ecddbc71828e66b8b9aa2b83b72
Richard Palmer <rich@rdjpalmer.com>
2020-06-11 08:18:33 +0100

    Fix default meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
+import Head from "next/head";
 import { useEffect } from "react";
 
 import "../styles/_app.css";
+
+const Color = {
+  light: ["#008040", "#0000E0", "#DB0A5B", "#E00000"],
+  dark: [
+    "#00FF7F",
+    "#36D7B7",
+    "#FC6399",
+    "#9370DB",
+    "#FFD700",
+    "#F9690E",
+    "#F64747",
+  ],
+};
+
+function getColor(isDarkMode) {
+  if (isDarkMode) {
+    const limit = Color.dark.length;
+    const index = Math.floor(Math.random() * limit);
+    return Color.dark[index];
+  }
+
+  const limit = Color.light.length;
+  const index = Math.floor(Math.random() * limit);
+  return Color.light[index];
+}
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -15,9 +41,34 @@ export default function App({ Component, pageProps }) {
     }
   }, []);
 
+  const lightColor = getColor(false);
+  const darkColor = getColor(true);
+
   return (
-    <div className="container">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="Creator of Timo & Byozo. Product focused engineer."
+        />
+        <meta name="author" content="Richard Palmer" />
+        <style>
+          {`
+            a { color: ${lightColor}; }
+            @media (prefers-color-scheme: dark) {
+              a { color: ${darkColor}; }
+            }
+          `}
+        </style>
+      </Head>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
