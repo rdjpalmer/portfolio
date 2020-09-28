69d6d2d1c06f1fe04fd17fee3d50cae9f95957cb
Richard Palmer <rich@rdjpalmer.com>
2020-09-28 10:12:34 +0100

    Fix meta data generation

--- generate-metadata.js
+++ generate-metadata.js
@@ -24,105 +24,75 @@ const rssDefaultValues = {
   author: "Richard Palmer",
 };
 
-// var fs = require('fs');
-// var path = require('path');
-var walk = function(dir, done) {
-  var results = [];
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var pending = list.length;
-    if (!pending) return done(null, results);
-    list.forEach(function(file) {
-      file = path.resolve(dir, file);
-
-      fs.stat(file, function(err, stat) {
-        if (stat && stat.isDirectory()) {
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            if (!--pending) done(null, results);
-          });
-        } else {
-          results.push(file);
-          if (!--pending) done(null, results);
-        }
-      });
-    });
-  });
-};
-
-console.log(path.resolve("./"));
-
-walk(path.resolve("./"), (error, results) => {
-  const log = results.filter(
-    (result) =>
-      !(
-        result.includes(".git") ||
-        result.includes("node_modules") ||
-        result.includes(".next/cache")
-      )
-  );
-  console.log(log);
-});
+const VERCEL_URL = process.env.VERCEL_URL;
+const siteUrl = VERCEL_URL || "https://rdjpalmer.com";
+const pagePath = VERCEL_URL
+  ? "./next/serverless/pages"
+  : "./.next/server/pages/";
 
 async function generateMetaData() {
-  // try {
-  //   const sitemapJson = [
-  //     {
-  //       loc: "https://rdjpalmer.com/",
-  //       changefreq: "weekly",
-  //       priority: "1",
-  //     },
-  //   ];
-  //   const feed = new RSS({
-  //     title: "Richard Palmer, Creator of Timo and Byozo",
-  //     description: "Creator of Timo & Byozo. Product focused engineer.",
-  //     site_url: "https://rdjpalmer.com",
-  //     feed_url: "https://rdjpalmer.com/feed.xml",
-  //   });
-  //   Object.entries(prerenderManifest.routes).map(([slug, routeConfig]) => {
-  //     // const segments = routeConfig.dataRoute.split("/");
-  //     // const jsonFileName = segments[segments.length - 1];
-  //     // const pageConfigPath = path.resolve(`./.next/server/pages/${jsonFileName}`);
-  //     if (slug !== "/") {
-  //       console.log(routeConfig);
-  //       const pageConfig = require(path.resolve(pageConfigPath));
-  //       const pageSitemapConfig =
-  //         sitemapConfiguration[slug] || sitemapConfiguration.fallback;
-  //       const location = `https://rdjpalmer.com${slug}`;
-  //       sitemapJson.push({
-  //         url: omitBy(
-  //           {
-  //             loc: location,
-  //             lastmod: pageConfig.pageProps.updated || null,
-  //             ...pageSitemapConfig,
-  //           },
-  //           (value) => value == null
-  //         ),
-  //       });
-  //       feed.item({
-  //         ...rssDefaultValues,
-  //         title: pageConfig.pageProps.title,
-  //         guid: pageConfig.pageProps.slug,
-  //         url: location,
-  //         date: pageConfig.pageProps.updated,
-  //         description: pageConfig.pageProps.description,
-  //       });
-  //     }
-  //   });
-  //   const sitemapXml = dedent`
-  //     <?xml version="1.0" encoding="UTF-8"?>
-  //     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  //       ${jsontoxml(sitemapJson)}
-  //     </urlset>
-  //   `;
-  //   await Promise.all([
-  //     writeFile("./.next/static/sitemap.xml", sitemapXml),
-  //     writeFile("./.next/static/rss.xml", feed.xml()),
-  //   ]);
-  // } catch (error) {
-  //   console.error("Unable to generate site metadata", error);
-  //   throw new Error(error);
-  // }
+  try {
+    const sitemapJson = [
+      {
+        loc: siteUrl,
+        changefreq: "weekly",
+        priority: "1",
+      },
+    ];
+
+    const feed = new RSS({
+      title: "Richard Palmer, Creator of Timo and Byozo",
+      description: "Creator of Timo & Byozo. Product focused engineer.",
+      site_url: siteUrl,
+      feed_url: `${siteUrl}/feed.xml`,
+    });
+
+    Object.entries(prerenderManifest.routes).map(([slug, routeConfig]) => {
+      const segments = routeConfig.dataRoute.split("/");
+      const jsonFileName = segments[segments.length - 1];
+      const pageConfigPath = path.resolve(`${pagePath}${jsonFileName}`);
+
+      const pageConfig = require(path.resolve(pageConfigPath));
+      const pageSitemapConfig =
+        sitemapConfiguration[slug] || sitemapConfiguration.fallback;
+      const location = `${siteUrl}${slug}`;
+
+      sitemapJson.push({
+        url: omitBy(
+          {
+            loc: location,
+            lastmod: pageConfig.pageProps.updated || null,
+            ...pageSitemapConfig,
+          },
+          (value) => value == null
+        ),
+      });
+
+      feed.item({
+        ...rssDefaultValues,
+        title: pageConfig.pageProps.title,
+        guid: pageConfig.pageProps.slug,
+        url: location,
+        date: pageConfig.pageProps.updated,
+        description: pageConfig.pageProps.description,
+      });
+    });
+
+    const sitemapXml = dedent`
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${jsontoxml(sitemapJson)}
+      </urlset>
+    `;
+
+    await Promise.all([
+      writeFile("./.next/static/sitemap.xml", sitemapXml),
+      writeFile("./.next/static/rss.xml", feed.xml()),
+    ]);
+  } catch (error) {
+    console.error("Unable to generate site metadata", error);
+    throw new Error(error);
+  }
 }
 
 generateMetaData();
