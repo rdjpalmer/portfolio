b1655d38c0b6104508d93e1b98572fc02acbfe77
Richard Palmer <rich@rdjpalmer.com>
2020-11-01 15:31:16 +0000

    WIP

--- scripts/build-open-graph/driver.js
+++ scripts/build-open-graph/driver.js
@@ -1,16 +1,39 @@
 const chromium = require("chrome-aws-lambda");
+const path = require("path");
 
 const exePath =
   process.platform === "win32"
     ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
     : process.platform === "linux"
     ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+    : path.resolve(
+        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+      );
+
+async function getOptions(isDev) {
+  let options;
+  if (isDev) {
+    options = {
+      args: [],
+      args: chromium.args,
+      // defaultViewport: chromium.defaultViewport,
+      executablePath: path.resolve(
+        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+      ),
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  } else {
+    options = {
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    };
+  }
+
+  return options;
+}
 
 module.exports = async function getDriver(isDev) {
-  return chromium.puppeteer.launch({
-    args: chromium.args,
-    executablePath: isDev ? exePath : await chromium.executablePath,
-    headless: isDev ? true : chromium.headless,
-  });
+  return chromium.puppeteer.launch(getOptions(isDev));
 };
